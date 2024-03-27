2439ef293ac384bb454dd6fcbc6fbb275cea8f48
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-27 21:19:20 +0600

    array te string prin korano, length dekha

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 // Array in Javascript
 
-let marks=[45,54,67,87,98,33];
-console.log(marks[3]); //87
-console.log(marks);
-console.log(marks.length);
+// let marks=[45,54,67,87,98,33];
+// console.log(marks[3]); //87
+// console.log(marks);
+// console.log(marks.length);
 // marks[2], eshob diye colsole a value change kora jay array te, string a vuleo jaito na, notun arekta string khola lagto
 // string-> immuateable->change kora jay na
 // array-> mutable-> change kora jay
+
+
+
+let heroes=["spiderman", "superman", "batman", "hulk", "ironman"];
+console.log(heroes);
+console.log(heroes.length);
