bbc82759c0b5b527abb51034c3131bbd67745f6c
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-28 19:35:56 +0600

    calculating average marks

--- script.js
+++ script.js
@@ -28,11 +28,28 @@
 
 
 // for of loop
-let heroes=["spiderman", "superman", "batman", "hulk", "ironman"];
+// let heroes=["spiderman", "superman", "batman", "hulk", "ironman"];
 
-for (hero of heroes){
-    console.log(hero.toUpperCase());
-}
+// for (hero of heroes){
+//     console.log(hero.toUpperCase());
+// }
+
+
+
+
+let marks=[85,97,44,37,76,60];
+
+sum=0;
+
+for (let value of marks){
+    sum=sum+value;
+    //console.log(sum);
+ }
+ let average= sum/marks.length;
+ console.log(`average marks of the class is ${average}`);
+// let totalMarks=85+97+44+37+76+60;
+// let averageMarks=totalMarks/6;
+// console.log(averageMarks);
 
 
 
