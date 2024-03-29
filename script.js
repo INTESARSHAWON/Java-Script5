fc71b6c733018ec65bec4071f5e9819157183751
INTESARSHAWON <intesarshawon@gmail.com>
2024-03-29 19:18:23 +0600

    pop()

--- script.js
+++ script.js
@@ -116,4 +116,7 @@ fruits.push("banana", "lemon", "watermelon", "jackfruit");
 //eigula sob last a push hoise,ebar pop korbo, pop last theke hoy, pop return value-o dey..., array change kore fele eigula...,
 console.log(fruits);
 
+let deletedFruits= fruits.pop();
+console.log("deleted fruits=",deletedFruits);
+console.log(fruits);
 
